eca6f96852a9579535586ce49f44b0e6d4fc9862
topdingpu <472667562@qq.com>
2019-06-01 16:25:14 +0800

    修改一些问题

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,7 +16,7 @@ angular.module("angular-animate", []).directive("animatectrl", [function() {
     angular.module("SensationApp", ["ngRoute", "ngAnimate", "ngSanitize", "ui.bootstrap", "easypiechart", "textAngular", "ui.tree", "ngMap", "ngTagsInput", "slick", "ui.select", "selectApp", "ui.calendar", "tablesorting", "inlineedittable", "TodoApp", "app.controllers", "app.directives", "app.localization", "app.nav", "app.ui.ctrls", "app.ui.directives", "app.form.validation", "app.ui.form.ctrls", "app.ui.form.directives", "app.tables", "app.task", "app.chart.ctrl", "app.chart.directives", "angular-animate", "app.calendar", "app.ui.jvectormap", "xeditable", "FullscreenApp", "galleryApp", "datatables"]).config(["$routeProvider",
         function($routeProvider) {
             var routes, setRoutes;
-            return routes = ["dashboard/dashboard-v1", "ui/typography", "ui/buttons", "ui/icons", "ui/grids", "ui/widgets", "ui/components", "ui/timeline", "ui/nested-lists", "ui/fontawesome", "ui/animation", "ui/panel", "ui/xeditable", "maps/gmap", "maps/vectormap", "tables/static", "tables/dynamic", "tables/responsive", "forms/elements", "forms/layouts", "forms/validation", "forms/select", "forms/wizard", "charts/flot", "charts/morris", "charts/highcharts", "pages/404", "pages/500", "pages/blank", "pages/forgot-password", "pages/invoice", "pages/lock-screen", "pages/profile", "pages/signin", "pages/signup", "pages/directory", "pages/faq", "pages/gallery", "mail/compose", "mail/inbox", "mail/mailview", "tasks/tasks", "calender/calender", "ngtable/ngtable", "settings/settings-panel"],
+            return routes = [],
                 setRoutes = function(route) {
                     var config, url;
                     return url = "/" + route,
